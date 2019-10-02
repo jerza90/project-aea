9419529dbc623af7be111bc911a35499c8efc2ae
azrecarsome <azre@carsome.my>
2019-10-03 01:03:39 +0800

    add table product histories

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    public function histories(){
+        return $this->hasOne(Product_history::class,'product_code','product_code');
+    }
 }
